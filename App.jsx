6a552073c56794cdd536de6579b7fc00dfe09368
Harsh-sjce <130289605+Harsh-sjce@users.noreply.github.com>
2024-03-02 11:36:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BackGround from './components/BackGround'
+import ForGround from './components/ForGround'
+
+
+function App() {
+  return (<>
+    <div className='relative w-full h-screen bg-zinc-800'>
+   <BackGround/>
+   <ForGround/>
+    </div>
+    </>
+  )
+}
+
+export default App
